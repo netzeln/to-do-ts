c39ee1c0d73f550cbda3f0f1546b0fb0c1c48115
nic <netzeln@gmail.com>
2016-03-21 09:53:22 -0700

    create basic to-do

--- app/to-do.js
+++ app/to-do.js
@@ -1,3 +1,8 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var Task = (function () {
     function Task(description, priority) {
         this.description = description;
@@ -9,9 +14,43 @@ var Task = (function () {
     };
     return Task;
 }());
+var HomeTask = (function (_super) {
+    __extends(HomeTask, _super);
+    function HomeTask() {
+        _super.apply(this, arguments);
+    }
+    return HomeTask;
+}(Task));
+var HobbyTask = (function (_super) {
+    __extends(HobbyTask, _super);
+    function HobbyTask(description) {
+        _super.call(this, description, "low");
+        this.description = description;
+    }
+    return HobbyTask;
+}(Task));
+var WorkTask = (function (_super) {
+    __extends(WorkTask, _super);
+    function WorkTask(dueDate, description, priority) {
+        _super.call(this, description, priority);
+        this.dueDate = dueDate;
+        this.description = description;
+        this.priority = priority;
+    }
+    return WorkTask;
+}(Task));
 var tasks = [];
-tasks.push(new Task("Do JS Homework", "High"));
-tasks.push(new Task("Watch Daredevil", "low"));
-tasks.push(new Task("Eat Sardines", "High"));
-tasks[0].markDone();
+tasks.push(new HomeTask("Do JS Homework", "High"));
+tasks.push(new HomeTask("Eat Sardines", "High"));
+tasks.push(new HomeTask("Install Hose Holder", "low"));
+tasks.push(new HobbyTask("Watch Daredevil"));
+tasks.push(new HobbyTask("play on computer"));
+var today = new Date();
+var tomorrow = new Date();
+tomorrow.setDate(today.getDate() + 1);
+var nextDay = new Date();
+nextDay.setDate(today.getDate() + 2);
+tasks.push(new WorkTask(today, "Finish this lesson", "high"));
+tasks.push(new WorkTask(tomorrow, "learn Angular", "high"));
+tasks.push(new WorkTask(nextDay, "group work", "medium"));
 console.log(tasks);
--- app/to-do.ts
+++ app/to-do.ts
@@ -6,9 +6,36 @@ class Task {
   }
 }
 
+class HomeTask extends Task {}
+
+class HobbyTask extends Task {
+  constructor(public description: string){
+    super(description, "low");
+  }
+}
+
+class WorkTask extends Task {
+  constructor(public dueDate: Date, public description: string, public priority: string){
+    super(description, priority);
+  }
+}
+
 var tasks =[];
-tasks.push(new Task("Do JS Homework", "High"));
-tasks.push(new Task("Watch Daredevil", "low"));
-tasks.push(new Task("Eat Sardines", "High"));
-tasks[0].markDone();
+tasks.push(new HomeTask("Do JS Homework", "High"));
+tasks.push(new HomeTask("Eat Sardines", "High"));
+tasks.push(new HomeTask("Install Hose Holder", "low"));
+
+tasks.push(new HobbyTask("Watch Daredevil"));
+tasks.push(new HobbyTask("play on computer"));
+
+var today = new Date();
+var tomorrow = new Date();
+tomorrow.setDate(today.getDate() + 1);
+var nextDay = new Date();
+nextDay.setDate(today.getDate() + 2);
+
+tasks.push(new WorkTask(today, "Finish this lesson", "high"));
+tasks.push(new WorkTask(tomorrow, "learn Angular", "high"));
+tasks.push(new WorkTask(nextDay, "group work", "medium"));
+
 console.log(tasks);
